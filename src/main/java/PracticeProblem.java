a8b5be45b19316b2601abb410061ddcb425ccdb9
JackyLui1215 <343344461@tdsb.ca>
2026-04-29 18:59:59 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,37 @@ public class PracticeProblem {
 
 	}
 
-}
+	public static int findValue(int num, int[] arr) {
+		int result = -1;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] ==num) {
+				result = i;
+				i = arr.length;
+			}
+		}
+		return result;
+	}
+
+	public static int findThirdValue(String word, String[] arr) {
+		int count = 0;
+			for (int i = 0; i < arr.length; i++) {
+			if (word.equals(arr[i])) {
+				count++;
+			}
+			if (count == 3) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static double[] append(double num, double[] arr) {
+		double[] arr2 = new double[arr.length + 1];
+		for (int i = 0; i < arr.length; i++) {
+			arr2[i] = arr[i];
+		}
+		arr2[arr.length] = num;
+		return arr2;
+	}
+	}
+
